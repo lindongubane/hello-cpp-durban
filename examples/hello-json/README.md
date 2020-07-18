b332c3d5c16d35f4f7809b359d474ff9f84e48d6
Matthew <matthew@sobantu.co.za>
2020-07-18 11:36:37 +0200

    Updated code to check where the required json file could be
    Added instructions for building from the VS command line.

--- examples/hello-json/README.md
+++ examples/hello-json/README.md
@@ -15,13 +15,20 @@ Once that is done, you can run the following command inside of the project direc
 
 To run the project:
 
+(Currently only tested on Windows, but should be similar on Linux)
 * The easy way:
     * Use CLion from JetBrains.
-* Almost easy:
-    * Use Visual Studio Code and its CMake extension to create a Visual Studio project in the build directory.
-* Other ways:
-    * TODO finish describing how to build the project using CMake.
-    * Maybe NetBeans?
+        * Make sure that it detects Visual Studio or another supported compiler on your system.
+    * Click the Run button to run the project.
+* The command line way (on Windows with Visual Studio 2019 installed with all related C/C++ and CMake options)
+    * Run the **Developer Command Prompt for VS 2019** (usually found in the Start Menu).
+        * OR open a command prompt in your folder and run ``"%programfiles(x86)%\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"``
+    * Then ``mkdir build``, which is our target directory for the VS solution and build outputs.
+    * Then ``cmake . -B build`` which creates a Visual Studio solution by default.
+    * Then ``msbuild build\hello_json.sln`` to compile the project.
+        * OR just open build\hello_json.sln in Visual Studio.
+    * Finally use ``build\bin\hello_json.exe`` to run the program.
+        * You can use ``cat someone.json`` to see the example file it generated, besides the command line output.
 
 ## Example Overview
 The **conanfile.py** defines all of the dependencies and copies the source files to the _packages_ directory.
--- examples/hello-json/src/main.cpp
+++ examples/hello-json/src/main.cpp
@@ -1,28 +1,61 @@
 #include <iostream>
 #include <fstream>
 #include <string_view>
+#include <filesystem>
+#include <optional>
 #include <nlohmann/json.hpp>
 
 #include "person.hpp"
 
+namespace fs = std::filesystem;
 // For more information about nlohmann json, go to https://github.com/nlohmann/json or https://nlohmann.github.io/json/.
 using json = nlohmann::json;
 
-int main()
+
+std::optional<fs::path> json_file_exists(std::string_view exe_path, std::string_view json_file)
 {
-    // Sample part 1
-    std::ifstream hello_file("hello.json");
-    auto hello_json = json::parse(hello_file);
-
-    std::cout << hello_json["mainMessage"].get<std::string_view>() << '\n';
-    std::cout << "Here are some places you might know:\n";
-
-    for (auto& city : hello_json["someCities"])
+    // If you run the program with an absolute path, or if you run it from inside its directory,
+    // current path with be the exact path where the executable is, and where our json file(s) should be.
+    if (auto path = fs::current_path().append(json_file); fs::exists(path))
     {
-        std::cout << "* " << city.get<std::string_view>() << '\n';
+        std::cout << path << "\n";
+        return path;
     }
 
-    std::cout << hello_json << '\n';
+    // Alternatively, if we have the first command line argument, this should tell us how the executable was invoked,
+    // and then we should get a (usually) relative path to the file we want.
+    if(auto otherPath = fs::path(exe_path).parent_path().append(json_file); fs::exists(otherPath))
+    {
+        std::cout << otherPath << "\n";
+        return otherPath;
+    }
+
+    return std::nullopt;
+}
+
+
+int main(int argc, const char** argv)
+{
+    // Sample part 1
+    if (auto path = json_file_exists(argv[0], "hello.json"); path.has_value())
+    {
+        std::ifstream hello_file(path.value());
+        auto hello_json = json::parse(hello_file);
+
+        std::cout << hello_json["mainMessage"].get<std::string_view>() << '\n';
+        std::cout << "Here are some places you might know:\n";
+
+        for (auto& city : hello_json["someCities"])
+        {
+            std::cout << "* " << city.get<std::string_view>() << '\n';
+        }
+
+        std::cout << hello_json << '\n';
+    }
+    else
+    {
+        std::cerr << "hello.json not found.\n";
+    }
 
     // Sample part 2
     hello::person someone = { "B.J.", "Blazkowicz", 69  };
