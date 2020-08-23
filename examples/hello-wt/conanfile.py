7334fd457f4f6caaf1064937f8fe827b6eba6699
Matthew <matthew@sobantu.co.za>
2020-08-23 12:53:20 +0200

    Added wt example

---
+++ examples/hello-wt/conanfile.py
@@ -0,0 +1,18 @@
+from conans import ConanFile, CMake, tools
+import os.path
+
+
+class HelloTomlConanFile(ConanFile):
+    build_requires = "cmake/3.17.3"
+    requires = "wt/4.4.0"
+    generators = "cmake"
+    build_folder = "build"
+        
+    def build(self):
+        self.build_folder = "build"
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def imports(self):
+        self.copy("Wt/resources/*", src="@bindirs", dst="")
